37fed475a7a717112d04961114e005cf1d13d812
Omarnael <omar.elkelesh@student.guc.edu.eg>
2021-01-12 01:38:15 +0200

    adds glow to weapons

--- Assets/Scripts/Weapons/WeaponsConstants.cs
+++ Assets/Scripts/Weapons/WeaponsConstants.cs
@@ -243,4 +243,24 @@ public static Vector3 SMG_AIM = ASSAULT_RIFLE_AIM;
 public static Vector3 SHOTGUN_AIM = new Vector3(0.55f,0.5f,0);
 
 public static Vector3 HUNTING_RIFLE_AIM = SHOTGUN_AIM;
+
+
+
+
+//SPAWN DATA
+  public static IDictionary<string, Vector3> SPAWN_ROTATIONS = new Dictionary<string,Vector3>() {
+	{"assaultRifle", new Vector3(90f,0,0)},
+  {"smg", new Vector3(0,0,90f)},
+  {"huntingRifle", new Vector3(90f,0,0)},
+  {"shotgun", new Vector3(90,0,0)}
+  };
+
+  public static IDictionary<string, Vector3> SPAWN_TRANSFORMATIONS = new Dictionary<string,Vector3>() {
+	{"assaultRifle", new Vector3(1f,1f,1f)},
+  {"smg", new Vector3(2f,2f,2f)},
+  {"huntingRifle", new Vector3(1.5f,1.5f,1.5f)},
+  {"shotgun", new Vector3(0.15f,0.15f,0.15f)}
+  };
+
 }
+
